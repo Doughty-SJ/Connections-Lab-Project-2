e4a8e91f51719d3f884c425da2657ea53815f512
Steven Doughty <sd155@ad.nyu.edu>
2020-11-05 21:26:08 +0400

    I hate GIT

--- Public/sketch.js
+++ Public/sketch.js
@@ -1,87 +1,135 @@
+//declare variables
+let playerID;
+let gamestate;
+let player;
+let playerList = [];
+let otherPlayerSprites = [];
+let playerPos;
+let test;
+var WALL_THICKNESS = 30;
+
 //open and connect socket
 let socket = io();
 
-
-//Listen for confirmation fo connection
-socket.on('connect', function(){
+//Listen for confirmation of connection
+socket.on('connect', function () {
   console.log("Connected");
+  playerID = socket.id;
+  socket.emit('newPlayer');
 });
 
+//Get gamestate data
+socket.on('state', function (obj) {
+  //console.log(obj);
+  gamestate = obj;
+  playerList = obj.playerList;
+})
 
-
-var WALL_THICKNESS = 30;
-
+//test = createSprite(200,200,50,50);
+//socket.broadcast.emit("test", test);
 
 function setup() {
+
+  console.log("setup");
+
+  frameRate(45);
   createCanvas(800, 400);
-  player = createSprite(width/2, height/2, 20, 50);
+
+
+  
+
+  
+
+  //Remove client from playerList
+  const index = playerList.indexOf(socket.id);
+  if (index > -1) {
+    playerList.splice(index, 1);
+  }
+
+  //Create sprites for other players -- maybe this needs to be done on every newplayer.
+  for (let i = 0; i < playerList.length; i++) {
+
+    console.log(gamestate.players[playerList[i]]);
+    otherPlayerSprites[playerList[i]] = createSprite(gamestate.players[playerList[i]].x, gamestate.players[playerList[i]].y, 20, 50);
+  }
+
+  //Set Client as active player and create sprite
+  player = gamestate.players[playerID];
+  player = createSprite(player.x, player.y, 20, 50);
   player.setDefaultCollider();
   player.maxSpeed = 5;
 
-  wallTop = createSprite(width/2, -WALL_THICKNESS/2, width+WALL_THICKNESS*2, WALL_THICKNESS);
+  //Create Static Barriers
+  wallTop = createSprite(width / 2, -WALL_THICKNESS / 2, width + WALL_THICKNESS * 2, WALL_THICKNESS);
   wallTop.immovable = true;
 
-  wallBottom = createSprite(width/2, height+WALL_THICKNESS/2, width+WALL_THICKNESS*2, WALL_THICKNESS);
+  wallBottom = createSprite(width / 2, height + WALL_THICKNESS / 2, width + WALL_THICKNESS * 2, WALL_THICKNESS);
   wallBottom.immovable = true;
 
-  wallLeft = createSprite(-WALL_THICKNESS/2, height/2, WALL_THICKNESS, height);
+  wallLeft = createSprite(-WALL_THICKNESS / 2, height / 2, WALL_THICKNESS, height);
   wallLeft.immovable = true;
 
-  wallRight = createSprite(width+WALL_THICKNESS/2, height/2, WALL_THICKNESS, height);
+  wallRight = createSprite(width + WALL_THICKNESS / 2, height / 2, WALL_THICKNESS, height);
   wallRight.immovable = true;
-
-  //Listen for sprite "data" from the server
-  socket.on('data', function(obj) {
-    console.log(obj);
-    player;
-  });
 
 }
 
 function draw() {
   background(255, 255, 255);
 
+//testing emit on sprites
+test = createSprite(200, 200, 50, 50);
+socket.broadcast.emit("test", test);
 
 
-
-//Movement Keycode W = 87 , A = 65 , S = 83, D = 68
-  if(keyDown("A")){
-    player.rotation -= 4;
-    
+  //Send player Sprite position object when key is pressed
+  if (keyIsPressed === true) {
+    socket.emit('data', playerPos);
   }
-  if(keyDown("D")){
+
+  //Movement Keycode W = 87 , A = 65 , S = 83, D = 68
+  if (keyDown("A")) {
+    player.rotation -= 4;
+  }
+  if (keyDown("D")) {
     player.rotation += 4;
   }
-  if(keyDown("W")){
+  if (keyDown("W")) {
     player.addSpeed(0.2, player.rotation);
-  }else{
+  } else {
     player.setSpeed(0.0);
   }
 
+  //Position and ID data for Client-player
+  playerPos = { x: player.position.x, y: player.position.y, ID: playerID };
 
-    //all sprites bounce at the screen edges
-    for(var i=0; i<allSprites.length; i++) {
-      var s = allSprites[i];
-      if(s.position.x<0) {
-        s.position.x = 1;
-        s.velocity.x = abs(s.velocity.x);
-      }
-  
-      if(s.position.x>width) {
-        s.position.x = width-1;
-        s.velocity.x = -abs(s.velocity.x);
-      }
-  
-      if(s.position.y<0) {
-        s.position.y = 1;
-        s.velocity.y = abs(s.velocity.y);
-      }
-  
-      if(s.position.y>height) {
-        s.position.y = height-1;
-        s.velocity.y = -abs(s.velocity.y);
-      }
+
+
+  //all sprites bounce at the screen edges
+  for (var i = 0; i < allSprites.length; i++) {
+    var s = allSprites[i];
+    if (s.position.x < 0) {
+      s.position.x = 1;
+      s.velocity.x = abs(s.velocity.x);
     }
+
+    if (s.position.x > width) {
+      s.position.x = width - 1;
+      s.velocity.x = -abs(s.velocity.x);
+    }
+
+    if (s.position.y < 0) {
+      s.position.y = 1;
+      s.velocity.y = abs(s.velocity.y);
+    }
+
+    if (s.position.y > height) {
+      s.position.y = height - 1;
+      s.velocity.y = -abs(s.velocity.y);
+    }
+  }
+
+
 
   //draw all the sprites added to the sketch so far
   //the positions will be updated automatically at every cycle
@@ -89,8 +137,8 @@ function draw() {
 }
 
 
-  //Send Sprite position object
-  socket.emit('data', player);
+
+
 
 
 
--- index.js
+++ index.js
@@ -11,17 +11,35 @@ server.listen(port, () => {
     console.log("Server listening at port: " + port);
 });
 
+
+
+//Random Integer Generator credit - https://www.w3schools.com/js/js_random.asp
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+//Game State Object
+const gameState = {
+    playerList: [],
+    players: {}
+}
+
 //Initialize socket.io
 let io = require('socket.io').listen(server);
 
 //Listen for individual clients/users to connect
-io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', function (socket) {
     console.log("We have a new client: " + socket.id);
 
     //Listen for a message named 'data' from this client
-    socket.on('data', function(data) {
+    socket.on('data', function (data) {
+
         //Data can be numbers, strings, objects
-        console.log("Received: 'data' " + data);
+        //console.log("Received: 'data' " + data.x +":"+data.y+data.ID);
+        gameState.players[data.ID].x = data.x;
+        gameState.players[data.ID].y = data.y;
+        
 
         //Send the data to all clients, including this one
         //Set the name of the message to be 'data'
@@ -34,8 +52,32 @@ io.sockets.on('connection', function(socket) {
         // socket.emit('data', data);
     });
 
+    socket.on('newPlayer', () => {
+        gameState.players[socket.id] = { x: getRndInteger(50, 750), y: getRndInteger(50, 350) }
+        gameState.playerList.push(socket.id);
+       
+        console.log(gameState);
+    })
+
+
     //Listen for this client to disconnect
-    socket.on('disconnect', function() {
+    socket.on('disconnect', function () {
         console.log("A client has disconnected: " + socket.id);
+        delete gameState.players[socket.id]
+
+        //Remove from playerList
+        const index = gameState.playerList.indexOf(socket.id);
+        if (index > -1) {
+            gameState.playerList.splice(index, 1);
+        }
+
+
     });
 });
+
+
+
+setInterval(function () {
+    io.sockets.emit('state', gameState);
+    //console.log(gameState.players);
+}, 1000 / 10);
